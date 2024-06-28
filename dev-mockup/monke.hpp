8370c68f6603e986671ff0e5178afb73fa6defa3
Michael Wozniak <woobe-studio@proton.me>
2024-06-28 22:45:55 +0100

    added monkey types, spaces + other classes

--- dev-mockup/monke.hpp
+++ dev-mockup/monke.hpp
@@ -6,6 +6,24 @@
 
 class Space;
 class Animal;
+class Rarity;
+class Monkey;
+class Note;
+class MedicalNote;
+class BehavioralNote;
+class OtherNote;
+class ProboscisMonkey;
+class GuineaBaboon;
+class NightMonkey;
+class Lemur;
+class Zoo;
+class Enclosure;
+class Cage;
+class Hospital;
+class User;
+class AuthDaemon;
+class LoggingDaemon;
+
 
 class Zoo{
 	private:
@@ -39,3 +57,126 @@ class Cage : public Space{};
 class Hospital : public Space{};
 
 class Enclosure : public Space{};
+
+
+enum class Rarity {
+    Common,
+    Uncommon,
+    Rare,
+    Legendary,
+    Epic
+};
+
+class Animal {
+protected:
+    Space* space;
+    std::string name;
+    int age;
+    Rarity rarity;
+
+public:
+    void setSpace(Space* zoo);
+    Space* getSpace();
+    void setName(const std::string& animalName);
+    std::string getName();
+    void setAge(int animalAge);
+    int getAge();
+};
+
+class Monkey : public Animal {
+};
+
+class Note {
+private:
+    Animal* animal;
+    long date;
+    std::string note;
+
+public:
+    Animal* getAnimal();
+    long getDate();
+    std::string getNote();
+};
+
+class MedicalNote : public Note {
+};
+
+class BehavioralNote : public Note {
+};
+
+class OtherNote : public Note {
+};
+
+class ProboscisMonkey : public Monkey {
+};
+
+class GuineaBaboon : public Monkey {
+};
+
+class NightMonkey : public Monkey {
+};
+
+class Lemur : public Monkey {
+};
+
+
+namespace Security {
+    class User {
+    private:
+        std::string username;
+        std::string password;
+        int accType;
+        std::vector<Zoo*> accessToZoo;
+
+    public:
+        std::string getUsername();
+        void setUsername(const std::string& username);
+        std::string getPassword();
+        void setPassword(const std::string& password);
+        Zoo* getZoo();
+    };
+
+    class AuthDaemon {
+    private:
+        static AuthDaemon* instance;
+        std::vector<User*> users;
+        User* loggedInUser;
+
+    private:
+        AuthDaemon();
+
+    public:
+        static AuthDaemon* getInstance();
+        void login(User* user);
+        void login(const std::string& username, const std::string& password);
+        void logout(User* user);
+        bool isLoggedIn(User* user);
+        bool isLoggedInUserAdmin();
+        void addUser(User* user);
+        void remUser(User* user);
+    };
+}
+
+namespace Utility {
+    class LoggingDaemon {
+    private:
+        static LoggingDaemon* instance;
+        bool enabledAction;
+        bool enabledAuth;
+        bool enabledAdminActions;
+
+    private:
+        LoggingDaemon();
+
+    public:
+        static LoggingDaemon* getInstance();
+        void logAction(const std::string& action);
+        void logAuth(Security::User* user);
+        bool getEnabledAction();
+        bool getEnabledAuth();
+        bool getEnabledAdminActions();
+        void setEnabledAction(bool value);
+        void setEnabledAuth(bool value);
+        void setEnabledAdminActions(bool value);
+    };
+}
