b245a55d92aee63f7c9a46b1c8adfed774de5515
Michael Wozniak <woobe-studio@proton.me>
2024-07-01 16:03:41 +0200

    added zoo implementation

---
+++ src/zoo.cpp
@@ -0,0 +1,49 @@
+#include "monke.hpp"
+
+namespace Monke{
+Zoo::Zoo(){
+	std::vector<Space*> spaces;
+}
+Zoo::~Zoo(){
+	for (auto iterSpaces : spaces){
+		delete iterSpaces;
+	}
+}
+
+int Zoo::setZooName(std::string newName){
+	this->name=newName;
+	return 0;
+}
+
+std::string Zoo::getZooName(){
+	return this->name;
+}
+
+int Zoo::addSpace(Space *spaceToAdd){
+	try{
+		spaces.push_back(spaceToAdd);
+	}
+	catch(...) {
+		return -20;
+	}
+	return 0;
+}
+
+
+int Zoo::removeSpace(Space *spaceToRemove){
+	std::vector<Space*>::iterator iterSpaces = spaces.begin();
+	while(iterSpaces != spaces.end()){
+		if(spaceToRemove==(spaces)[iterSpaces-spaces.begin()]){
+			try{
+			(spaces).erase(iterSpaces);
+			} catch (...) {return -20;}
+			return 0;
+		}
+		iterSpaces++;
+	}
+	return -6;
+}
+
+
+
+}
