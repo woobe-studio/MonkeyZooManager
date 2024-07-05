a610a77490f10deefc07983bd28a2d53569e8ced
Michael Wozniak <woobe-studio@proton.me>
2024-07-05 22:27:20 +0200

    added enclosure.cpp

---
+++ src/enclosure.cpp
@@ -0,0 +1,56 @@
+#include "monkey.hpp"
+
+namespace Monkey
+{
+    Enclosure::Enclosure()
+    {
+        openTime.hour = 0;
+        openTime.minute = 0;
+        closeTime.hour = 0;
+        closeTime.minute = 0;
+        visitorSpots = 0;
+        temperature = 0.0f;
+    }
+
+    Enclosure::~Enclosure() {}
+
+    hourMin Enclosure::getOpenTime()
+    {
+        return openTime;
+    }
+
+    hourMin Enclosure::getCloseTime()
+    {
+        return closeTime;
+    }
+
+    int Enclosure::getVisitorSpots()
+    {
+        return visitorSpots;
+    }
+
+    float Enclosure::getTemperature()
+    {
+        return temperature;
+    }
+
+    void Enclosure::setOpenTime(hourMin newOpenTime)
+    {
+        openTime = newOpenTime;
+    }
+
+    void Enclosure::setCloseTime(hourMin newCloseTime)
+    {
+        closeTime = newCloseTime;
+    }
+
+    void Enclosure::setVisitorSpots(int newVisitorSpots)
+    {
+        visitorSpots = newVisitorSpots;
+    }
+
+    void Enclosure::setTemperature(float newTemperature)
+    {
+        temperature = newTemperature;
+    }
+}
