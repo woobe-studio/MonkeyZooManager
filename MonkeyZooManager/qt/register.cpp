98fe69ceb798e580e676e48f2e2ce78a505d0da5
Michael Wozniak <woobe-studio@proton.me>
2024-07-22 16:11:01 +0200

    preparing for register implementation

---
+++ MonkeyZooManager/qt/register.cpp
@@ -0,0 +1,48 @@
+#include "register.h"
+#include "ui_register.h"
+
+#include <QMessageBox>
+#include <QTimer>
+
+#include "../src/monkey.hpp"
+
+RegisterForm::RegisterForm(QWidget* parent)
+    : QMainWindow(parent), ui(new Ui::RegisterForm)
+{
+    ui->setupUi(this);
+    setMinimumSize(360, 640);
+    resize(360, 640);
+}
+
+RegisterForm::~RegisterForm()
+{
+    delete ui;
+}
+
+void RegisterForm::on_pushButtonRegister_clicked()
+{
+    QString username = ui->lineEditUsername->text();
+    QString password = ui->lineEditPassword->text();
+
+    if (CheckTakenData(username.toStdString(), password.toStdString())) {
+        QMessageBox* msgBox = new QMessageBox(QMessageBox::Information, "Login", "Login successful!", QMessageBox::Ok, this);
+
+        connect(msgBox, &QMessageBox::buttonClicked, [this, msgBox](QAbstractButton*) {
+            ui->lineEditUsername->setText("");
+            ui->lineEditPassword->setText("");
+            emit RegisterSuccessful();
+            msgBox->deleteLater();
+            });
+
+        msgBox->exec();
+    }
+    else {
+        QMessageBox::warning(this, "Login", "Invalid username or password.");
+    }
+}
+
+bool RegisterForm::CheckTakenData(const std::string& login, const std::string& password)
+{
+    Monkey::AuthDaemon* authorizationDaemon = Monkey::AuthDaemon::getInstance();
+    return (bool)authorizationDaemon->login(login, password);
+}
---
+++ MonkeyZooManager/qt/register.h
@@ -0,0 +1,29 @@
+#ifndef REGISTERFORM_H
+#define REGISTERFORM_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class RegisterForm; }
+QT_END_NAMESPACE
+
+class RegisterForm : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit RegisterForm(QWidget* parent = nullptr);
+    ~RegisterForm();
+
+signals:
+    void RegisterSuccessful();
+
+private slots:
+    void on_pushButtonRegister_clicked();
+    bool CheckTakenData(const std::string& login, const std::string& password);
+
+private:
+    Ui::RegisterForm* ui;
+};
+
+#endif // REGISTERFORM_H
